4d3b23dd78eaf6c78689d5fdbe754fa7c20c570a
Tran Cong Tru <trutc@innotech-vn.com>
2022-10-14 16:58:36 +0700

    1

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -3,11 +3,12 @@ module "eks" {
   version = "18.26.6"
 
   cluster_name    = local.cluster_name
-  cluster_version = "1.22"
+  cluster_version = "1.23"
 
   vpc_id     = module.vpc.vpc_id
-  subnet_ids = module.vpc.private_subnets
-
+  //subnet_ids = module.vpc.private_subnets
+  subnet_ids = module.vpc.public_subnets
+  
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
 
--- security-groups.tf
+++ security-groups.tf
@@ -3,12 +3,12 @@ resource "aws_security_group" "node_group_one" {
   vpc_id      = module.vpc.vpc_id
 
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
+    from_port = 0
+    to_port   = 0
+    protocol  = "-1"
 
     cidr_blocks = [
-      "10.0.0.0/8",
+      "0.0.0.0/0",
     ]
   }
 }
